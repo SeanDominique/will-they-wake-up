accaaecdacf5760d702b3b9bafb1ff864cc99950
Mario Rocha <mario.luis.rocha@outlook.com>
2024-11-28 21:38:34 +0100

    updated data.py

--- wtwu/packages/data.py
+++ wtwu/packages/data.py
@@ -239,13 +239,18 @@ def sampling_EEGs(list_of_EEGs, fs=100, sampling_rate=600, sampling_size=15,hour
 
             splits.append(EEG[(sampling_rate*i)*fs:((sampling_rate*i)+sampling_size)*fs])
 
-            if hours != None:
-                print((hours[i_EEG])+((fs/sampling_rate)*i))
-                split_time.append((hours[i_EEG])+((fs/sampling_rate)*i))
+            if hours.shape != None:
+
+
+                if i_EEG < len(hours):
+                    split_time.append((hours[i_EEG])+((fs/sampling_rate)*i))
 
             i += 1
 
         i_EEG += 1
+
+
+
 
     splits = np.array(splits)
     if len(split_time)>0:
@@ -265,8 +270,10 @@ def sample_all(reduced_array, fs=100, sampling_rate=600, sampling_size=15,hours=
     list_of_splits = np.array(list_of_splits)
     list_of_splits = list_of_splits.reshape(int(list_of_splits.shape[0]),8,int(list_of_splits.shape[1]/8),int(list_of_splits.shape[2]))
     list_of_splits = np.transpose(list_of_splits,axes=(0,2,3,1))
+    list_of_splits = list_of_splits.reshape(list_of_splits.shape[0]*list_of_splits.shape[1],list_of_splits.shape[2],list_of_splits.shape[3])
     if len(list_of_split_times)>0:
         list_of_split_times = np.array(list_of_split_times)
+        list_of_split_times = list_of_split_times[0,:]
 
     return list_of_splits, list_of_split_times
 
@@ -313,19 +320,27 @@ def get_psds(EEG_list,fs=100, mode='channels', hours=None,input_type='array'):
             return None
 
     if input_type == 'array':
-        psds_ar = np.zeros((EEG_list.shape[1],int(fs/2)))
-        for i in range(EEG_list.shape[1]):
-            f, psds_ar[i,:] = welch(EEG_list[i,:], fs=fs, nperseg=1024)
-        psds_df = pd.DataFrame(psds_ar)
+        psds = []
+        for i in range(0, EEG_list.shape[0]):
+            psds1 = []
+            for j in range(0, EEG_list.shape[2]):
+                f, psds_temp = welch(EEG_list[i,:,j], fs=fs, nperseg=4*fs)
+                psds1.append(psds_temp)
+            psds.append(psds1)
+        psds_ar = np.array(psds)
+        psds_ar = np.transpose(psds_ar, axes=(0,2,1))
 
-        if mode == 'time' and hours.shape[1] == psds_ar.shape[1]:
-            psds_df = pd.concat([psds_df, hours_df], axis=1)
-            psds_df = psds_df.groupby(by='hours',as_index=True).mean()
-            return psds_df, f
-        elif mode == 'time':
-            return psds_df, f
+
+
+        if mode == 'time':
+            psds_df = pd.DataFrame(psds_ar)
+            if hours.shape[0] == psds_ar.shape[1]:
+                psds_df = pd.concat([psds_df, hours_df], axis=1)
+                psds_df = psds_df.groupby(by='hours',as_index=True).mean()
+                return f, psds_df
+            return f, psds_df
         elif mode == 'channels':
-            return psds_ar, f
+            return f, psds_ar
         else:
             print('get_psds: unrecognised mode.')
             return None
