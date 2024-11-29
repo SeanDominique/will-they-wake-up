8c3586ae093593ce840d7656f4c65de4e687494d
Mario Rocha <mario.luis.rocha@outlook.com>
2024-11-29 15:34:48 +0100

    added times to get_and_treat.py and fixed data.py

--- get_and_treat.py
+++ get_and_treat.py
@@ -8,7 +8,7 @@ import sys
 #list_of_patients = ([name for name in os.listdir(training_folder) if name.isnumeric()])
 patient = sys.argv[1] # input string or int in CLI, assumes 0 at the start of the patient number if < 1000
 #for patient in list_of_patients:
-if not os.path.exists(f'./data/processed/{patient}/headers.pkl'):
+if not os.path.exists(f'./data/processed/{patient}/times.npy'):
 
     survived, eeg_data_headers, all_eeg_data = wtstorage.import_data(patient)
 
@@ -16,6 +16,14 @@ if not os.path.exists(f'./data/processed/{patient}/headers.pkl'):
     if eeg_data_headers != 'Error' and len(eeg_data_headers)>0:
 
         fs = eeg_data_headers[0]['fs']
+
+        # get all initial recording times for each file analysed.
+        hours = []
+        for header in eeg_data_headers:
+            hours.append(header['recording_hour'])
+        hours = np.array(hours).astype(np.float16)
+
+        print(hours)
         if not os.path.exists(f'./data/processed/{patient}'):
             os.makedirs(f'./data/processed/{patient}')
 
@@ -30,8 +38,15 @@ if not os.path.exists(f'./data/processed/{patient}/headers.pkl'):
                                             original_freq=fs
                                             )
 
+
+
         # splits TS into 15 second windows every 10 minutes
-        list_of_splits, list_of_times = wtdata.sample_all(reduced_eeg_data,hours=None)
+        list_of_splits, list_of_times = wtdata.sample_all(reduced_eeg_data,hours=hours)
+        std = np.std(list_of_splits,axis=0)
+        mean = np.mean(list_of_splits, axis=0)
+        std = np.where(std == 0, 1, std)
+        for i in range(0,list_of_splits.shape[0]):
+            list_of_splits[i,:,:] = (list_of_splits[i,:,:] - mean ) / std
 
         # creates PSDs of these 15s windows
         psds_fs, list_of_psds = wtdata.get_psds(list_of_splits)
@@ -56,6 +71,10 @@ if not os.path.exists(f'./data/processed/{patient}/headers.pkl'):
         # headers.pkl contains a list with all the headers for each array in time_splits.npy
         with open(f'./data/processed/{patient}/headers.pkl', 'wb') as f:
             pickle.dump(eeg_data_headers, f)
+
+        # times.npy has all timestamps for each observation in time_splits.npy
+        with open(f'./data/processed/{patient}/times.npy', 'wb') as f:
+            np.save(f, list_of_times)
 
         print(patient, ' done!')
     else:
